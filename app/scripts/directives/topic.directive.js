24fb64289942c86d0b7baa76134da6b61d12e256
Alison Smith <alison.smith@dac.us>
2018-08-08 11:41:33 -0400

    add info to loading indicator to remind user that model is loading, remove pre-task questionnaire

--- app/scripts/directives/topic.directive.js
+++ app/scripts/directives/topic.directive.js
@@ -65,9 +65,6 @@ angular.module('itmUiApp').directive('topic', ['$sce', '$mdDialog', 'TopicServic
         console.log("blur!");
       };
 
-      scope.chipsChanged = function($event) {
-        console.log('chips changed', $event);
-      }
 
       /**
        * Method called when a user chooses to delete the topic
